bebd8928e15f9d4d77c448786b0f97c35aa3a9a2
NeelanshaAgarwal <na1027@srmist.edu.in>
2025-02-12 22:02:22 +0530

    todo list

---
+++ Beginners Project 2/src/components/Todo.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react"
+import "../style.css";
+
+const Todo = () => {
+
+    const[todos, setTodos] = useState([])
+    const[input, setInput] = useState("")
+
+    const handleSubmit = () => {
+        setTodos((todos)=>{
+            return todos.concat({
+                text: input,
+                id: Math.floor(Math.random())*10,
+            });
+        });
+        setInput("");
+    };
+
+    
+
+    const removeTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+
+  return (
+    <div className="container">
+      <input 
+      type="text" 
+      placeholder="New Todo"
+      value={input}
+      onChange={e => setInput(e.target.value)}
+      />
+
+
+      <button onClick={handleSubmit}>Submit</button>
+
+      <ul className="todos-list">
+            {todos.map((todo) => (
+                <li className="todo" key={todo.id}>
+                    <span>
+                        {todo.text}
+                    </span>
+                    <button className="close" onClick={()=>removeTodo(id)}>-</button>
+                    </li>
+            ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Todo
