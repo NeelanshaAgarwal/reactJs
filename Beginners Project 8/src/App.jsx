c83033767e5199a0c7c4ea29289ea70add8db706
NeelanshaAgarwal <na1027@srmist.edu.in>
2025-02-16 18:58:28 +0530

    Set up new React project with Vite, including main files, components, and styling

---
+++ Beginners Project 8/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Accordion from './components/Accordion'
+import { accordionData } from './utils/content'
+
+const App = () => {
+  return (
+    <div className='accordion'>
+      {accordionData.map(({ title, content }) => (
+      <Accordion title={title} content={content}/>
+      ))}
+    </div>
+  )
+}
+
+export default App
